7bffe1619c9d8e47eb08de827d9324f6f928ab7d
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2023-12-18 09:23:14 +0200

    Create AppSettingsReader

---
+++ InventoryManagementSystem/AppSettingsReader.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace InventoryManagementSystem
+{
+    public class AppSettingsReader
+    {
+        private readonly IConfiguration _configuration;
+        public AppSettingsReader(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetConnectionString(string key)
+        {
+            var connectionString = _configuration.GetConnectionString(key);
+            if (connectionString == null)
+            {
+                throw new Exception($"No Connection String found for key '{key}'");
+            }
+            return connectionString;
+        }
+    }
+}
--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -1,5 +1,14 @@
 ﻿using InventoryManagementSystem;
+using Microsoft.Extensions.Configuration;
 
+IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+AppSettingsReader appSettingsReader = new(configuration);
+
+Console.WriteLine(appSettingsReader.GetConnectionString("MSDB"));
 Inventory inventory = new Inventory();
 InventorySeeder inventorySeeder = new InventorySeeder(inventory);
 inventorySeeder.SeedInventory();
